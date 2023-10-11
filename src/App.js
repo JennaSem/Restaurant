20ce18ecda6c65b0804a73889a517c56861ed693
Denis Semenov <denissemenov@Deniss-Air.attlocal.net>
2023-10-11 16:05:38 -0700

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { data } from "./data";
+import { imageData } from "./data";
+import "./App.css";
 
 function App() {
+  const [restaurant, setRestaurant] = useState(data);
+  const [showText, setShowText] = useState(false);
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const removeRestaurant = (id) => {
+    let newRestaurant = restaurant.filter((item) => item.id !== id);
+    setRestaurant(newRestaurant);
+  };
+
+  const showTextClick = (item) => {
+    item.showMore = !item.showMore;
+    setShowText(!showText);
+  };
+
+  const previousImage = () => {
+    setCurrentImage((prevImage) =>
+      prevImage === 0 ? imageData.length - 1 : prevImage - 1
+    );
+  };
+
+  const nextImage = () => {
+    setCurrentImage((prevImage) =>
+      prevImage === imageData.length - 1 ? 0 : prevImage + 1
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box">
+      <div className="center-content">
+        <h1>Los Angeles Top {restaurant.length} Restaurants</h1>
+      </div>
+
+      {restaurant.map((item) => {
+        const { id, name, image, description, showMore } = item;
+
+        return (
+          <div key={id}>
+            <div>
+              <h3>
+                {id} - {name}
+              </h3>
+            </div>
+
+            <div className="container">
+              <img src={image} width="300px" height="350px" alt={restaurant} />
+            </div>
+
+            <div className="container">
+              <p>
+                {showMore ? description : description.substring(0, 220) + "..."}
+                <button className="btnShow" onClick={() => showTextClick(item)}>
+                  {" "}
+                  {showMore ? "Show less" : "Show more"}
+                </button>
+              </p>
+            </div>
+
+            <div className="container">
+              <button className="btn" onClick={() => removeRestaurant(id)}>
+                Remove
+              </button>
+            </div>
+          </div>
+        );
+      })}
+
+      <div className="container">
+        <button className="delete" onClick={() => setRestaurant([])}>
+          Delete All
+        </button>
+      </div>
+
+      <div className="container">
+        <h2>More cozy places</h2>
+
+        <img
+          src={imageData[currentImage].addImage}
+          width="300px"
+          height="350px"
+          alt="restaurant"
+        />
+
+        <div className="btn-container">
+          <button onClick={previousImage}>Previous</button>
+          <button onClick={nextImage}>Next</button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,105 @@
+export const data = [
+  {
+    id: 1,
+    name: "El Compadre",
+    image:
+      "https://images.unsplash.com/photo-1657545215947-a45e4975dd58?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cmVzdHVyYW50JTIwbWV4c2ljYW58ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
+    description: `We specialize in showcasing "The fine foods of Mexico". We serve family recipes handed down over generations using select cuts of meats, and choice seafood alongside season fresh vegetables. Over the years we have annexed and expanded our restaurant and services, complete with our 2004 opening of El Compadre Restaurant 7 miles east of Hollywood located in the hip area of Echo Park. Our newest addition opened in 2014 in the heart of Downtown Los Angeles across from the Staples Center and Convention Center. Today, El Compadre Restaurant is still family owned and operated.`,
+  },
+
+  {
+    id: 2,
+    name: "n/naka",
+    image:
+      "https://images.unsplash.com/photo-1616671276441-2f2c277b8bf9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bWljaGVsaW4lMjByZXN0YXVyYW50fGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+    description: `Crafted through the Japanese culinary art form of kaiseki, n/naka’s modern interpretation presents the very best seasonal ingredients from both California and Japan’s land and sea.  Our tasting menu celebrates ingredients at the peak of its perfection and reimagines kaiseki through a California lens.`,
+  },
+
+  {
+    id: 3,
+    name: "Providence",
+    image:
+      "https://images.unsplash.com/photo-1625262423584-10dfe111b3e0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bWljaGVsaW4lMjByZXN0YXVyYW50fGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+    description: `Chef Cimarusti is completely dedicated to procuring the finest sustainable seafood—from regional coasts and international waters—and treats those ingredients with uncompromising respect and sophisticated technique. “First and foremost, what guides us here is sustainability,” Cimarusti says. “We use only wild-caught, sustainable products, mostly from American waters, and look to highlight their finest qualities.”`,
+  },
+
+  {
+    id: 4,
+    name: "Raffaello Ristorante",
+    image:
+      "https://images.unsplash.com/photo-1532117472055-4d0734b51f31?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aXRhbGlhbiUyMHJlc3RhdXJhbnR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
+    description: `Raffaello Ristorante, in the heart of old San Pedro, is a family-run Italian Restaurant. The best Italian food in the South Bay! Started by our Italian immigrant parents nearly forty years ago, the family tradition carries on today with fantastic food at phenomenal prices. Come on in! We feed you like you are family.`,
+  },
+
+  {
+    id: 5,
+    name: "Brent's Deli Northridge",
+    image:
+      "https://images.unsplash.com/photo-1555992336-fb0d29498b13?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGFtZXJpY2FuJTIwcmVzdGF1cmFudHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    description: `At Brent’s Deli, we combine family values with quality ingredients for a recipe of long-standing success. Voted as one of the best delicatessens in Los Angeles and Southern California, the story of Brent’s Deli begins well before we opened our doors in 1967.`,
+  },
+
+  {
+    id: 6,
+    name: "Langer's",
+    image:
+      "https://images.unsplash.com/photo-1515711127392-4c62a99c3393?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGFtZXJpY2FuJTIwcmVzdGF1cmFudHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    description: `From its humble roots to its current success, Langer’s Delicatessen-Restaurant of Los Angeles, California, is the quintessential story of classic American enterprise.
+    The story begins in 1905, with the arrival at Ellis Island of Harry and Rose Langer, from Odessa, Russia. A tailor by trade, Harry settles with Rose in Newark, New Jersey. They have three children, all boys: Joe, Al and Morris. Harry is an observant Jew who must work on the Sabbath (Saturday) in order to support his family. As soon as the boys are old enough, they begin working in local businesses.`,
+  },
+
+  {
+    id: 7,
+    name: "Bubba Gump Shrimp Co.",
+    image:
+      "https://images.unsplash.com/photo-1663481086442-cc868578a415?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YnViYmElMjBndW1wJTIwc2hyaW1wJTIwY29tcGFueXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    description: `Bubba Gump Shrimp Co. captures the charm and American spirit that made “Forrest Gump” a smash hit, featuring down-home décor reminiscent of the movie’s setting in Alabama. Now branded as an American icon, the restaurant showcases memorabilia and still-photos from the movie, along with reproductions of script pages, storyboards and even some costumes.`,
+  },
+
+  {
+    id: 8,
+    name: "Maccheroni Republic",
+    image:
+      "https://maccheronirepublic.com/wp-content/uploads/2021/11/Restaurant.jpg",
+    description: `Maccheroni Republic is an all inclusive place where everyone is always welcome, the place where the wind of passion meets the knowledge of one of the most expert pasta makers on earth…we just don’t make pasta, we make magic happen. After years of research on the field we’ve selected exceptional variety of grains prioritizing non refined whole wheat flour harvested from the best farmers in the US. We took advantage of the locally sourced organic produce from various farmers market across California.`,
+  },
+
+  {
+    id: 9,
+    name: "Café Gratitude Venice",
+    image:
+      "https://plus.unsplash.com/premium_photo-1664970900025-1e3099ca757a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2FmZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    description: `These are the principles and values that guide us in every aspect of our business. We live these beliefs each day, which is what sets us apart. With them, we are able to provide our employees with a work environment where love and gratitude are the stars that guide us, creating an experience unlike any other we know of, and that is our goal. After all, we are not just in the restaurant business, we are in the business of developing people. Of all the successes we've found, we are most proud of the lives that have been changed through our communities and culture.`,
+  },
+];
+
+export const imageData = [
+  {
+    addImage:
+      "https://images.unsplash.com/photo-1509042239860-f550ce710b93?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FmZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  },
+
+  {
+    addImage:
+      "https://images.unsplash.com/photo-1559305616-3f99cd43e353?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y2FmZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    addImage:
+      "https://images.unsplash.com/photo-1445116572660-236099ec97a0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGNhZmV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
+  },
+
+  {
+    addImage:
+      "https://images.unsplash.com/photo-1559925393-8be0ec4767c8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2FmZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  },
+
+  {
+    addImage:
+      "https://images.unsplash.com/photo-1619474387533-301ed3b5a734?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTJ8fGJlYXR1ZnVsJTIwY2FmZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  },
+
+  {
+    addImage:
+      "https://images.unsplash.com/photo-1601379342258-6f1e994c575c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTF8fGJlYXR1ZnVsJTIwY2FmZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  },
+];
